a72c99ce0b25e0ae61b6082a7be8bafdd33942d9
jayluvsdivn <jayluvsdivn@gmail.com>
2019-04-06 11:56:20 -0700

    adds data from json to table, adds styling, adds button link to google maps

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-$(document).ready(function() {
-// write your code here
-
-});
+$(document).ready(function(){
+    $.getJSON('data.json', function(result){
+        $.each(result, function(i, name, description, location){
+            var name = result[i].name;
+            var description = result[i].description;
+            var location = result[i].location;
+            $("#tableData").append(`<tr><td class="name">${name}</td><td class="description">${description}</td><td class="location"><input type="button" onclick='location.href="https://www.google.com/maps/search/?api=1&query=${location[0]},${location[1]}";' value="Open in Google Maps"></td></tr>`);
+        })
+    })
+})
