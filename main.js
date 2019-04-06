4692cd06e8589651a00fb673e8daedfd1105dd37
jayluvsdivn <jayluvsdivn@gmail.com>
2019-04-06 15:40:55 -0700

    adds better styling, fixes header at the bottom bug

--- main.js
+++ main.js
@@ -4,7 +4,10 @@ $(document).ready(function(){
             var name = result[i].name;
             var description = result[i].description;
             var location = result[i].location;
-            $("#tableData").append(`<tr><td class="name">${name}</td><td class="description">${description}</td><td class="location"><input type="button" onclick='location.href="https://www.google.com/maps/search/?api=1&query=${location[0]},${location[1]}";' value="Open in Google Maps"></td></tr>`);
-        })
-    })
+            $("#tableData").append(`<tr><td class="name">${name}</td><td class="description">
+            ${description}</td><td class="location"><input type="button" 
+            onclick='location.href="https://www.google.com/maps/search/?api=1&query=${location[0]},${location[1]}";'
+            value="Open in Google Maps"></td></tr>`);
+        });
+    });
 })
